6408cd208c3e829f5c72f5cb0bf5d44453b988ea
JLosada-Dev <jd.losada.dev@gmail.com>
2025-05-08 10:12:38 -0500

    feat: add MilkProduction, BovineEvent, and MilkPrediction routes to the API

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
+// src/routes/index.js
 const express = require('express');
 
 const BovineRouter = require('./bovine.router');
+const MilkProductionRouter = require('./milkProduction.router');
+const BovineEventRouter = require('./bovineEvent.router');
+const MilkPredictionRouter = require('./milkPrediction.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
 
-  // Rutas de bovinos
+  //bovinos
   router.use('/bovines', BovineRouter);
+  //producción de leche
+  router.use('/milk-productions', MilkProductionRouter);
+  //eventos de bovino
+  router.use('/bovine-events', BovineEventRouter);
+  //predicciones de leche
+  router.use('/predictions', MilkPredictionRouter);
 }
-
 
 module.exports = routerApi;
